a59a5ac37c22e8b91438ba678f488de566801a3e
Bochao <david.yu0728@gmail.com>
2017-09-11 16:18:08 +0200

    End CH13.2

--- game_functions.py
+++ game_functions.py
@@ -32,12 +32,13 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, alien, bullets):
 
     screen.fill(ai_settings.bg_color)
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
+    alien.blitme()
     pygame.display.flip()
 
 def update_bullets(bullets):
